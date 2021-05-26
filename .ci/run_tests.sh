78ab5466467665051ea33f182acfcd63a685b01f
Datong Sun <datong.sun@konghq.com>
2021-05-26 01:57:39 -0700

    ci(actions) remove use of KBT based build and caching for CI runs

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 function cyan() {
     echo -e "\033[1;36m$*\033[0m"
@@ -12,17 +12,20 @@ export BUSTED_ARGS="--no-k -o htest -v --exclude-tags=flaky,ipv6,squid"
 
 if [ "$KONG_TEST_DATABASE" == "postgres" ]; then
     export TEST_CMD="bin/busted $BUSTED_ARGS,cassandra,off"
-    psql -v ON_ERROR_STOP=1 -h ${KONG_TEST_PG_HOST} -U ${KONG_TEST_PG_USER} -d ${KONG_TEST_PG_DATABASE} <<-EOSQL
+
+    psql -v ON_ERROR_STOP=1 -h localhost --username "$KONG_TEST_PG_USER" <<-EOSQL
         CREATE user ${KONG_TEST_PG_USER}_ro;
         GRANT CONNECT ON DATABASE $KONG_TEST_PG_DATABASE TO ${KONG_TEST_PG_USER}_ro;
         \c $KONG_TEST_PG_DATABASE;
         GRANT USAGE ON SCHEMA public TO ${KONG_TEST_PG_USER}_ro;
         ALTER DEFAULT PRIVILEGES FOR ROLE $KONG_TEST_PG_USER IN SCHEMA public GRANT SELECT ON TABLES TO ${KONG_TEST_PG_USER}_ro;
 EOSQL
+
 elif [ "$KONG_TEST_DATABASE" == "cassandra" ]; then
     export KONG_TEST_CASSANDRA_KEYSPACE=kong_tests
     export KONG_TEST_DB_UPDATE_PROPAGATION=1
     export TEST_CMD="bin/busted $BUSTED_ARGS,postgres,off"
+
 else
     export TEST_CMD="bin/busted $BUSTED_ARGS,postgres,cassandra,db"
 fi
